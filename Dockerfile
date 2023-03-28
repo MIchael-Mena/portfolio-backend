c1f53e45b792a850f2f640f6960c26ad4cde7a61
Michael Mena <michael.mena2408@gmail.com>
2023-03-28 16:02:03 -0300

    u

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:20-jdk
 MAINTAINER MichaelMena
 COPY target/portfolio-backend.jar portfolio-backend.jar
-EXPOSE 8080
 ENTRYPOINT ["java","-jar","/portfolio-backend.jar"]
--- src/main/java/com/portfolioCRUD/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolioCRUD/portfolio/PortfolioApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"com.portfolioCRUD.portfolio.*"})
 @EntityScan(basePackages = {"com.portfolioCRUD.portfolio.*"})
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PortfolioApplication {
 
 

98dc2714b3dc51f7209dcd0c2ebc1831abc0bf59
Michael Mena <michael.mena2408@gmail.com>
2023-03-28 00:27:28 -0300

    u

--- src/main/java/com/portfolioCRUD/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolioCRUD/portfolio/PortfolioApplication.java
@@ -2,12 +2,20 @@ package com.portfolioCRUD.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
+/*@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.portfolioCRUD.portfolio.Model"})*/
 @SpringBootApplication
 public class PortfolioApplication {
 
+//	private static ConfigurableApplicationContext applicationContext;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
+//		PortfolioApplication.applicationContext = SpringApplication.run(PortfolioApplication.class, args);
 	}
 
 }

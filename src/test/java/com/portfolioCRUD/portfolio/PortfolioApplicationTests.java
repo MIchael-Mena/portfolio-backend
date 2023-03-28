dd1d06f562a29a0cdadef41504fb9ae2ee55a6d2
Michael Mena <michael.mena2408@gmail.com>
2023-03-27 21:43:29 -0300

    u

--- src/test/java/com/portfolioCRUD/portfolio/PortfolioApplicationTests.java
+++ src/test/java/com/portfolioCRUD/portfolio/PortfolioApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfolioCRUD.portfolio;
+/*package com.portfolioCRUD.portfolio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class PortfolioApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/

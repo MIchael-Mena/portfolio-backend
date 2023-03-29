c50f6cdd01c6da0bc1d1ac2180f893f1e1d2f123
Michael Mena <michael.mena2408@gmail.com>
2023-03-29 00:09:46 -0300

    u

--- src/main/java/com/portfolioCRUD/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolioCRUD/portfolio/PortfolioApplication.java
@@ -3,18 +3,20 @@ package com.portfolioCRUD.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Configuration
 @ComponentScan(basePackages = {"com.portfolioCRUD.portfolio.*"})
 @EnableJpaRepositories(basePackages = {"com.portfolioCRUD.portfolio.*"})
 @EntityScan(basePackages = {"com.portfolioCRUD.portfolio.*"})
 @EnableAutoConfiguration
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {RepositoryRestMvcAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class PortfolioApplication {
 
 

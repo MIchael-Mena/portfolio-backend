4ca96d8ffe66a2d3236731ffdc733a299e963bbe
Michael Mena <michael.mena2408@gmail.com>
2023-05-15 20:00:26 -0300

    update

--- src/main/java/com/portfolioCRUD/portfolio/model/AboutMe.java
+++ src/main/java/com/portfolioCRUD/portfolio/model/AboutMe.java
@@ -1,8 +1,6 @@
 package com.portfolioCRUD.portfolio.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
